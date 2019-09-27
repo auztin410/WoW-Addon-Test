b31ea0a84e86f86c675b1fc644ead754d0746951
sugarcube <auztin410@gmail.com>
2019-09-26 20:09:20 -0400

    Initial commit

---
+++ Sandbox.lua
@@ -0,0 +1,10 @@
+Sandbox = { }
+
+function Sandbox:HelloWorld()
+    message("Hello World!")
+end
+
+function Sandbox:HideGryphons()
+    MainMenuBarLeftEndCap:Hide()
+    MainMenuBarRightEndCap:Hide()
+end
